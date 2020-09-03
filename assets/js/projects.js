3e7e39e670b1cd97a574f6cfd823bf78ccc2c178
w <wvvwvvvvwvvw@gmail.com>
2020-09-03 04:25:48 -0700

    projects js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,12 +1,130 @@
-import "../css/app.scss"
+import {create_element} from "./util"
+import Modal from "./modals"
 
 import "phoenix_html"
 
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
+const hooks = {}
+hooks.add_project = {
+  mounted() {
+    btn_add_project.addEventListener("click", () => {
+      const selected_encoder = select_encoder.value
+
+      const options = {}
+      for (const param_name of Object.keys(encoders[selected_encoder])) {
+        const param = encoders[selected_encoder][param_name]
+        const e = document.getElementById(`opt_${selected_encoder}_${param_name}`)
+        if (param.requires) {
+          const req_e = document.getElementById(`opt_${selected_encoder}_${param.requires}`)
+          if (param.requires_values.includes(req_e)) continue
+        }
+        if (param_name == "resolution") {
+          const res_dims = e.value.split("x")
+          options["--width"] = res_dims[0]
+          options["--height"] = res_dims[1]
+        } else {
+          options[param_name] = e.value
+        }
+      }
+      
+      const params = []
+      for (const param_name of Object.keys(options)) {
+        if (options[param_name].length > 0) {
+          if (param_name.startsWith("--")) {
+            params.push(`${param_name}=${options[param_name]}`)
+          } else if (param_name.startsWith("-")) {
+            params.push(param_name)
+            params.push(options[param_name])
+          }
+        } else {
+          params.push(param_name)
+        }
+      }
+      
+      const confirm_modal = new Modal({title: "Create Project"})
+      confirm_modal.show()
+
+      confirm_modal.get_body().style.textAlign = "center"
+
+      confirm_modal.confirm = create_element(confirm_modal, "button")
+      confirm_modal.confirm.textContent = "Confirm"
+      confirm_modal.confirm.focus()
+      confirm_modal.confirm.addEventListener("click", () => {
+        this.pushEvent("add_project", {files: [], params: params}, (reply, _ref) => {
+          confirm_modal.close()
+          if (reply.success) {
+          } else {
+            const err_modal = new Modal({title: "Error"})
+            const reason_t = create_element(err_modal, "div")
+            reason_t.textContent = "Reason:"
+            const reason = create_element(err_modal, "div")
+            reason.textContent = reply.reason
+            err_modal.show()
+          }
+        })
+      })
+    })
+  },
+
+  updated() {
+    console.log("Updated", this)
+  }
+}
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {hooks: hooks, params: {_csrf_token: csrfToken}})
 liveSocket.connect()
 
-export default liveSocket
+const encoders = JSON.parse(document.getElementById("encoders").textContent)
+
+for (const encoder_name of Object.keys(encoders)) {
+  for (const param_name of Object.keys(encoders[encoder_name])) {
+    const param = encoders[encoder_name][param_name]
+    const e = document.getElementById(`opt_${encoder_name}_${param_name}`)
+    if (param.requires) {
+      const req_e = document.getElementById(`opt_${encoder_name}_${param.requires}`)
+      const onchange = () => e.parentElement.classList.toggle("hidden", !param.requires_values.includes(req_e.value))
+      req_e.addEventListener("change", onchange)
+      onchange()
+    }
+  }
+}
+
+function show_params() {
+  for (const encoder_param of encoder_params.children) {
+    encoder_param.classList.toggle("hidden", encoder_param.id != `params_${select_encoder.value}`) 
+  }
+}
+
+select_encoder.addEventListener("change", () => {
+  show_params()
+})
+
+show_params()
+/*
+post(`${server}/api/add_project`, payload, e => {
+      const resp = JSON.parse(e)
+      confirm_modal.close()
+      if (!("success" in resp)) {
+        const err_modal = new Modal({title: "Error"})
+        const reason_t = create_element(err_modal, "div")
+        reason_t.textContent = "Reason:"
+        const reason = create_element(err_modal, "div")
+        reason.textContent = e
+        err_modal.show()
+      } else if (!resp["success"]) {
+        const err_modal = new Modal({title: "Error"})
+        const reason_t = create_element(err_modal, "div")
+        reason_t.textContent = "Reason:"
+        const reason = create_element(err_modal, "div")
+        reason.textContent = resp["reason"]
+        err_modal.show()
+      } else {
+        update_projects()
+        if (password.length > 0)
+          set_cookie(`pw-${server}`, password)
+      }
+    })*/
+console.log("ASD")
---
+++ assets/js/util.js
@@ -0,0 +1,82 @@
+function get(url, cb, fail) {
+  const r = new XMLHttpRequest()
+  r.open("GET", url)
+  r.onload = e => {
+    cb(r.responseText)
+  }
+  r.onerror = e => {
+    fail(e)
+  }
+  r.send(null)
+}
+
+function post(url, data, cb) {
+  const r = new XMLHttpRequest()
+  r.open("POST", url)
+  r.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+  r.onload = e => {
+    cb(r.responseText)
+  }
+  r.send(JSON.stringify(data))
+}
+
+const bytes_map = ["B", "K", "M", "G"]
+
+function n_bytes(bytes) {
+  if (bytes / 1024 < 1) return [bytes, 0]
+  let r = n_bytes(bytes / 1024)
+  return [r[0], r[1] + 1]
+}
+
+function bytes_str(bytes) {
+  const r = n_bytes(bytes)
+  return `${r[0].toFixed(1)}${bytes_map[r[1]]}`
+}
+
+function create_element(root, type, classes="") {
+  const e = document.createElement(type)
+  if (classes.length > 0) {
+    for (const class_name of classes.split(" "))
+      e.classList.toggle(class_name, true)
+  }
+  if (root)
+    root.appendChild(e)
+  return e
+}
+
+function create_field(label, root, element="input", type="", default_value="", min="", max="", step=1) {
+  const field = create_element(root, "div", "modal-row")
+  if (label.length > 0) {
+    field.label = create_element(field, "label")
+    field.label.htmlFor = `${label}_${counter()}`
+    field.label.textContent = label
+  }
+  field.input = create_element(field, element)
+  field.input.value = default_value
+  field.input.min = min
+  field.input.max = max
+  if (element == "input")
+    field.input.type = type
+  field.input.step = step
+  field.input.id = `${label}_${counter(true)}`
+
+  if (type == "number") {
+    field.input.classList.toggle("input_number", true)
+  }
+
+  if (type == "range") {
+    field.input.classList.toggle("range", true)
+    field.number = create_element(field, "span")
+    field.number.textContent = field.input.value
+    field.input.addEventListener("change", () => {
+      field.number.textContent = field.input.value
+    })
+    field.input.addEventListener("input", () => {
+      field.number.textContent = field.input.value
+    })
+  }
+
+  return field
+}
+
+export {get, post, bytes_str, create_element, create_field}

bebf12e32cb610ddd397a14547171b35b0c1ac1b
w <wvvwvvvvwvvw@gmail.com>
2020-09-25 16:26:18 -0700

    increase max upload size to 100mb

--- lib/grav1_web/endpoint.ex
+++ lib/grav1_web/endpoint.ex
@@ -44,7 +44,7 @@ defmodule Grav1Web.Endpoint do
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
   plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
+    parsers: [:urlencoded, {:multipart, length: 100_000_000}, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 

0a8a51cd8fe5976da0a3247738133f609d3889a3
w <wvvwvvvvwvvw@gmail.com>
2020-11-13 06:59:33 -0800

    re-order clients columns

--- lib/grav1_web/live/user.ex
+++ lib/grav1_web/live/user.ex
@@ -16,8 +16,7 @@ defmodule Grav1Web.SignUpLive do
   alias Grav1.User
 
   def render(assigns) do
-    changeset = User.changeset(%User{})
-    Grav1Web.UserView.render("sign_up.html", Map.put(assigns, :changeset, changeset))
+    Grav1Web.UserView.render("sign_up.html", assigns)
   end
 
   def handle_params(_, _, socket) do
